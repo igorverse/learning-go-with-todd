7db044a7f51b3d87483191ca24bd43ea94447eda
Igor Silva <osilvaigor@gmail.com>
2024-04-07 15:28:57 -0300

    feat: import puppy module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/igorverse/learning-go-with-todd
 
 go 1.22.1
+
+require github.com/igorverse/puppy v0.0.0-20240407182111-30f93c8016c5 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/igorverse/puppy"
+)
+
 func main() {
-	
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+
+	fmt.Println(s1)
+	fmt.Println(s2)
 }
