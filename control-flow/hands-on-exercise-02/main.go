4d0789898b6cdf03f8444875731bb63c09cd9a41
Igor Silva <osilvaigor@gmail.com>
2024-04-10 23:21:58 -0300

    feat: add hands on exercise 2

--- control-flow/hands-on-exercise-02/main.go
+++ control-flow/hands-on-exercise-02/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 func main() {
-	
+	x := rand.Intn(250)
+
+	fmt.Printf("O valor de x é %v\t", x)
+
+	switch {
+	case x <= 100:
+		fmt.Println("Entre 0 e 100")
+	case x >= 101 && x <= 200:
+		fmt.Println("Entre 101 e 200")
+	case x >= 201 && x <= 250:
+		fmt.Println("Entre 201 e 250")
+	default:
+		fmt.Println("Comportamento não esperado!")
+	}
 }
