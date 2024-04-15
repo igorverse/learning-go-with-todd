e53b4df019db3b3bfa4c69d124e08fc9305fd344
Igor Silva <osilvaigor@gmail.com>
2024-04-15 02:15:00 -0300

    feat: add closure example

---
+++ functions/closure/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	f := incrementor()
+	fmt.Println(f()) // 1
+	fmt.Println(f()) // 2
+	fmt.Println(f()) // 3
+	fmt.Println(f()) // 4
+	fmt.Println(f()) // 5
+	fmt.Println(f()) // 6
+	fmt.Println(f()) // 7
+}
+
+func incrementor() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
+}
