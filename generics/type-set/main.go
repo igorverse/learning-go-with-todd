afa3ad99f8f6ea926d66ae571b231bee81ec9025
Igor Silva <osilvaigor@gmail.com>
2024-04-21 19:54:33 -0300

    feat: add type alias

--- generics/type-set/main.go
+++ generics/type-set/main.go
@@ -11,17 +11,20 @@ func addF(a, b float64) float64 {
 }
 
 type myNumbers interface {
-	int | float64
+	~int | ~float64
 }
 
 func addT[T myNumbers](a, b T) T {
 	return a + b
 }
 
+type myAlias int
+
 func main() {
+	var n myAlias = 42
 	fmt.Println(addI(1, 2))
 	fmt.Println(addF(1.2, 2.2))
 
-	fmt.Println(addT(1, 2))
+	fmt.Println(addT(n, 2))
 	fmt.Println(addT(1.2, 2.2))
 }
