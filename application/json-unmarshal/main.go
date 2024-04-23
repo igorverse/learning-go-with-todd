0bbfc48c6c9fb33fea130d78437923b7fdb5bad3
Igor Silva <osilvaigor@gmail.com>
2024-04-23 18:39:39 -0300

    feat: add json unmarshal

---
+++ application/json-unmarshal/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	First string `json:"First"`
+	Last  string `json:"Last"`
+	Age   int    `json:"Age"`
+}
+
+func main() {
+	s := `[{"First":"James","Last":"Bond","Age":32},{"First":"Miss","Last":"Moneypenny","Age":27}]`
+	bs := []byte(s)
+
+	people := []Person{}
+
+	err := json.Unmarshal(bs, &people)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("all of the data", people)
+
+	for i, v := range people {
+		fmt.Println("\nPERSON NUMBER ", i)
+		fmt.Println(v.First, v.Last, v.Age)
+	}
+}
